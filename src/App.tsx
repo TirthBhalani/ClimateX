461be39ba8cf58bbc036019d1944fecc171c3edd
Tirth Bhalani <tirthbhalani12345@gmail.com>
2025-04-20 17:03:51 +0530

    API Config and endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ import { ThemeProvider } from './context/theme-provider';
 import { Layout } from './components/layout';
 import WeatherDashboard from './pages/weather-dashboard';
 import CityPage from './pages/city-page';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+const queryClient = new QueryClient()
 function App() {
   
 
   return (
     
+
+    <QueryClientProvider client={queryClient}>
+
     <BrowserRouter>
 
       <ThemeProvider>
@@ -23,6 +31,9 @@ function App() {
       </ThemeProvider>
 
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+
+    </QueryClientProvider>
 
     
   
